3c0acedc29e3998ff821d54b2de228359f378a35
Isak <isak.e.lindhe@gmail.com>
2014-05-11 23:51:06 +0200

    fortsatte implementera databas.

--- src/garage/BicycleGarageDatabase.java
+++ src/garage/BicycleGarageDatabase.java
@@ -3,34 +3,40 @@ package garage;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+
 public class BicycleGarageDatabase {
 	
-	HashMap<String,User> barcodemap;
-	HashMap<String,LinkedList<User>> whosPinIs;
-	LinkedList<RetrievalOrder> retrievalOrders;
+	HashMap<String,User> barcodeMap;
+	HashMap<String,LinkedList<User>> pinMap;
+	LinkedList<RetrievalOrder> orders;
 	
 	
 	
 	
 	public BicycleGarageDatabase(){
 		//TODO;
-		
+		barcodeMap = new HashMap<String, User>();
+		pinMap = new HashMap<String, LinkedList<User>>();
+		orders = new LinkedList<RetrievalOrder>();
 	}
 	public boolean checkBarcodeRegistered(String barcode){
-		//TODO;
-		return false;
+		return barcodeMap.containsKey(barcode);
 	}
 	public boolean checkPinRegistered(String pin){
-		//TODO;
-		return false;
+		return pinMap.containsKey(pin);
 	}
 	public boolean checkBikeRetrievable(String barcode){
-		//TODO;
+		
+		String thePin = barcodeMap.get(barcode).getPin();
+		for(RetrievalOrder o : orders){
+			//TODO;
+			if()
+		}
 		return false;
 	}
 	public User getUser(String barcode){
 		
-		return barcodemap.get(barcode);
+		return barcodeMap.get(barcode);
 	}
 	public void save(){
 		//TODO;
