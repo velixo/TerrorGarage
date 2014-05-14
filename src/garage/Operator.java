75d5bcfefd482f8ff05245553478e90d13cae665
DjungelJarl <shan.langlais@gmail.com>
2014-05-14 17:21:49 +0200

    hej

--- src/garage/Operator.java
+++ src/garage/Operator.java
@@ -392,7 +392,6 @@ public class Operator {
 			try {
 				Thread.sleep(4000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			continue;
